44732d8793b20ad4a00a5d118bcfe4a27cb8add5
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2025-01-07 23:19:17 +0000

    Ajout de la configuration Tailwind CSS, importation de polices et création d'icônes SVG, ainsi que d'un guide de style pour le front-end.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        dark_cyan: "hsl(158, 36%, 37%)",
+        cream: "hsl(30, 38%, 92%)",
+        Very_dark_blue: "hsl(212, 21%, 14%)",
+        Dark_grayish_blue: "hsl(228, 12%, 48%)",
+        White: "hsl(0, 0%, 100%)",
       },
     },
   },
